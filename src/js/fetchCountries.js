bd14c0342a6fbce9010734b2c2c11af9f0a36ca1
dbeshchuk <dmytro.beshchuk@protonmail.com>
2021-05-28 23:04:22 +0300

    Допил ДЗ

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,7 +2,9 @@ export default function fetchCountries(searchQuery) {
   return new Promise((resolve, reject) => {
     fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
       .then(response => {
-      if (response.status == 404) { reject }
+        if (response.status == 404) {
+          reject(new Error);
+        }
       
       resolve(response.json());
     })
